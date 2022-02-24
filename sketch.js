24987b7214b5d7742cbd5cb717afafa53806a88d
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-02-23 21:35:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+// criando a variavel do piso, e da imagem do piso
+
+// criando a variavel do menino, e da imagem do menino
+
+// criando a variavel do lado direito e lado esquerdo,
+
+
+
+function preload(){
+  // carregando imagem do piso
+
+  // carregando imagem do menino
+
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Fundo em movimento
+
+// criando sprite do piso
+
+// adcionando  imagem ao piso
+
+// dando velocidade 4 ao piso
+
+// dando scale 1.2 ao piso
+;
+
+
+// criando sprite do menino
+
+// adcionando  imagem ao menino
+
+
+// dando scale 0.08 ao menino
+
+  
+  
+// crie sprite borda (Limite) esquerdo
+ 
+ // torne a borda esquerda invisivel
+ 
+
+// crie sprite borda (Limite) direito
+ 
+ // torne a borda direita invisivel
+}
+function draw() {
+  background(0);
+  // de velocidade ao piso
+  
+  
+  // menino se movendo no eixe X com o mouse
+  
+  
+ 
+  // criando o sprite das paredes
+
+  // menino colidindo com as paredes
+ 
+  
+  
+  // fazendo um if pra deixar o piso com rolagem infinita, embrado que é a altura e não a largura.
+
+  
+  drawSprites();
+}
